8ad3f07abd872b2b5fb2345a7e6a58f48625e77f
Rasim <kayidlar@mail.ru>
2023-08-20 17:06:32 +0300

    search counterparty and invoice details

---
+++ Counterparty.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from datetime import datetime
+import requests
+
+CONFIG_PATH = 'C:\Rasim\Python\Config'
+sys.path.append(os.path.abspath(CONFIG_PATH))
+from configPrestige import DATA_AUTH
+
+
+def get_counterparty(tax_code):
+    counterparty = ''
+    url = (r"http://192.168.1.254/utp_prestige/odata/standard.odata/Catalog_Контрагенты?$format=json&$"
+           fr"filter=КодПоЕДРПОУ eq '{tax_code}'&$select=Ref_Key,Description")
+    resp = requests.get(url, auth=DATA_AUTH)
+    if resp.status_code == 200:
+        counterparty = resp.json()['value'][0].values()
+
+    return counterparty
+
+
+def get_contract_details(uuid):
+    result = ''
+    url = (r"http://192.168.1.254/utp_prestige/odata/standard.odata/Catalog_ДоговорыКонтрагентов?$format=json"
+           fr"&$inlinecount=allpages&$filter=Ref_Key eq guid'{uuid}'"
+           r"&$select=Description,_НКС_ДнівВідтермінуванняОплати")
+    resp = requests.get(url, auth=DATA_AUTH)
+    if resp.status_code == 200:
+        result = resp.json()['value'][0]
+
+    return result
+
+
+def get_list_of_fatura(date_fatura, client_uuid):
+    day = date_fatura.day
+    month = date_fatura.month
+    year = date_fatura.year
+
+    url = ("http://192.168.1.254/utp_prestige/odata/standard.odata/Document_НалоговаяНакладная?$format=json"
+           f"&$orderby=Date desc&$filter=Контрагент_Key eq guid'{client_uuid}'"
+           f"and year(Date) eq {year} "
+           f"and month(Date) eq {month} "
+           f"and day(Date) eq {day}"
+           "&$select=Number,Date,ДокументОснование")
+
+    resp = requests.get(url, auth=DATA_AUTH)
+    if resp.status_code == 200:
+        result = resp.json()['value']
+
+    return result
+
+if __name__ == '__main__':
+    tax_code = '425477026551'
+    get_counterparty(tax_code)
--- MergeExcleWord2.py
+++ MergeExcleWord2.py
@@ -6,24 +6,45 @@
 import json
 import pandas
 import pandas as pd
+from datetime import datetime
 import win32com.client as win32
 
 from mailmerge import MailMerge
 
+from Counterparty import get_counterparty, get_list_of_fatura
 from Word2Pdf import word_2_pdf
 
 
+def add_counterparty_name(path_to_file_excel):
+    df = pandas.read_excel(path_to_file_excel, sheet_name=0)
+    df['контрагент1С'] = None
+    df['контрагент1Сuuid'] = None
+    df['Дата складання ПН/РК'] = df['Дата складання ПН/РК'].dt.strftime('%d.%m.%Y')
+    df['Дата реєстрації ПН/РК в ЄРПН'] = df['Дата реєстрації ПН/РК в ЄРПН'].dt.strftime('%d.%m.%Y')
+    unq = df['Податковий номер Покупця'].unique().tolist()
+
+    for tax_code in unq:
+        client_uuid, client_name = get_counterparty(tax_code)
+        print(client_name)
+        df.loc[df['Податковий номер Покупця'] == tax_code, 'контрагент1С'] = client_name
+        df.loc[df['Податковий номер Покупця'] == tax_code, 'контрагент1Сuuid'] = client_uuid
+
+    return df  # df.astype(str)
+
+
 def merge_excel_and_word(path_to_file_excel):
-    df_source = pandas.read_excel(path_to_file_excel, sheet_name='source_raw')
-    # df_source['Порядковий № ПН/РК'].astype(str)
-    # df_source['Дата складання ПН/РК'].dt.strftime('%d.%m.%Y')
-    print(df_source)
-    df = pd.DataFrame()
-    df['tax_number'] = df_source['Порядковий № ПН/РК'].astype(str)
-    df['tax_date'] = df_source['Дата складання ПН/РК'].dt.strftime('%d.%m.%Y')
+    dfnew = pd.DataFrame()
+    df = pandas.read_excel(path_to_file_excel, sheet_name='source_raw')
+    df['counterparty'] = None
+    df['Дата складання ПН/РК'] = df['Дата складання ПН/РК'].dt.strftime('%d.%m.%Y')
+    df['Дата реєстрації ПН/РК в ЄРПН'] = df['Дата реєстрації ПН/РК в ЄРПН'].dt.strftime('%d.%m.%Y')
     for i, row in df.iterrows():
         df_list = []
         data = {}
+        tax_code = row['ІПН Покупця']
+        client_name = get_counterparty(tax_code)
+        print(client_name)
+        dfnew.loc[df['ІПН Покупця'] == row['ІПН Покупця'], 'counterparty'] = client_name
         df_list.append(row.to_dict())  # row.to_json(orient='records')
         data['columns'] = df_list
         data = json.dumps(data)
@@ -33,15 +54,42 @@ def merge_excel_and_word(path_to_file_excel):
 
         document.merge_rows('tax_number', data['columns'])
         document.merge_rows('tax_date', data['columns'])
-        word_file = fr'C:\Users\Rasim\Desktop\Scan\{i+1}.docx'
+        word_file = fr'C:\Users\Rasim\Desktop\Scan\{i + 1}.docx'
         document.write(word_file)  # saving file
 
-        pdf_file = fr'C:\Users\Rasim\Desktop\Scan\{i+1}.pdf'
+        pdf_file = fr'C:\Users\Rasim\Desktop\Scan\{i + 1}.pdf'
         word_2_pdf(word_file, pdf_file)
         if i == 10:
             break
 
 
+def get_contract(search_doc, list_doc):
+    for item in list_doc:
+        if str(search_doc) in item['Number']:
+            return item.values()
+        else:
+            continue
+
+
+def add_contract(df):
+    df['договор'] = None
+    for i, row in df.iterrows():
+        date_fatura = datetime.strptime(row['Дата складання ПН/РК'], '%d.%m.%Y')
+        client_uuid = row['контрагент1Сuuid']
+        list_of_fatura = get_list_of_fatura(date_fatura, client_uuid)
+        tax_doc = get_contract(row['Порядковий № ПН/РК'], list_of_fatura)
+        print(row['Порядковий № ПН/РК'], tax_doc)
+        pass
+    # print(list_of_fatura)
+
+
 if __name__ == '__main__':
     file_source = r"c:\Users\Rasim\Desktop\Scan\ТОВ ЄВРО СМАРТ ПАУЕР.xlsx"
-    merge_excel_and_word(file_source)
+    df = add_counterparty_name(file_source)
+    add_contract(df)
+    with pd.ExcelWriter(file_source, mode='a') as writer:
+        date = datetime.today().strftime("%d.%m.%Y")
+        df.to_excel(writer, sheet_name=date, index=False)
+
+    print(df)
+    # merge_excel_and_word(file_source)

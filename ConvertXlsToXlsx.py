7c31b456185b10190324a5ed2816446096d255b7
Rasim <kayidlar@mail.ru>
2023-08-23 14:34:38 +0300

    convert xls to xlsx - remove if exists file

--- ConvertXlsToXlsx.py
+++ ConvertXlsToXlsx.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import win32com.client as win32
 
@@ -8,10 +9,14 @@ def convert_xls_to_xlsx(xls_path) -> None:
     wb = excel.Workbooks.Open(path.absolute())
 
     # FileFormat=51 is for .xlsx extension
-    wb.SaveAs(str(path.absolute().with_suffix(".xlsx")), FileFormat=51)
+    save_to_xlsx_path = str(path.absolute().with_suffix(".xlsx"))
+    if os.path.isfile(save_to_xlsx_path):
+        os.remove(save_to_xlsx_path)
+    wb.SaveAs(save_to_xlsx_path, FileFormat=51)
     wb.Close()
     excel.Application.Quit()
-    return str(path.absolute().with_suffix(".xlsx"))
+    return save_to_xlsx_path
+
 
 if __name__ == '__main__':
     xls_path = r"c:\Users\Rasim\Desktop\Scan\ТОВ ЛЕГІОН 2015\Написать письмо\Копия ЛЕГІОН 2015.xls"

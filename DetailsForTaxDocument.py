0f4435470299dc28c5a7e73a54ccace040fb080a
Rasim <kayidlar@mail.ru>
2023-08-31 22:09:54 +0300

    added path to config file for different OS

--- DetailsForTaxDocument.py
+++ DetailsForTaxDocument.py
@@ -3,8 +3,14 @@ import sys
 from datetime import datetime
 import requests
 
-CONFIG_PATH = r'C:\Rasim\Python\Config'
+if os.environ['COMPUTERNAME'] == 'PRESTIGEPRODUCT':
+    CONFIG_PATH = r"d:\Prestige\Python\Config"
+    CONFIG_PATH_NOVAPOSHTA = r"D:\Prestige\Python\NovaPoshta"
+else:
+    CONFIG_PATH = r"C:\Rasim\Python\Config"
+    CONFIG_PATH_NOVAPOSHTA = r"c:\Rasim\Python\NovaPoshta"
 sys.path.append(os.path.abspath(CONFIG_PATH))
+
 from configPrestige import DATA_AUTH
 
 

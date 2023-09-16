55336226e17448322c55bba81a2a7400795d22f0
Rasim <kayidlar@mail.ru>
2023-09-16 14:25:20 +0300

    get ttn from 1c and add to df

--- MergeExcleWord.py
+++ MergeExcleWord.py
@@ -24,6 +24,7 @@ from pathvalidate import sanitize_filepath
 from datetime import datetime
 from mailmerge import MailMerge
 from DetailsForTaxDocument import get_counterparty, get_list_of_tax_fatura, get_contract_details, get_doc_sale_details
+from TTN import get_ttn_details
 from Word2Pdf import word_2_pdf
 from ConvertXlsToXlsx import convert_xls_to_xlsx
 
@@ -132,6 +133,22 @@ def doc_contract_details_add_to_df(df):
     return df
 
 
+def ttn_add_to_df(df):
+
+    df['1СномерТТН'] = None
+    df['1СдатаТТН'] = None
+    for i, row in df.iterrows():
+        doc_sale_uuid = row['invoice_key']
+        ttn_details = get_ttn_details(doc_sale_uuid)
+        if len(ttn_details) == 0:
+            continue
+        ttn_date = parse(ttn_details['Date']).strftime("%d.%m.%Y")
+        ttn_number = int(ttn_details['Number'])
+        df.loc[df['invoice_key'] == row['invoice_key'], '1СномерТТН'] = ttn_number
+        df.loc[df['invoice_key'] == row['invoice_key'], '1СдатаТТН'] = ttn_date
+
+    return df
+
 def doc_sale_details_add_to_df(df):
     df['год'] = None
     df['месяц'] = None
@@ -186,7 +203,6 @@ def create_new_excel(excel_file):
 def add_other_parameters_to_df(excel_file):
     df = pd.DataFrame()
     try:
-        excel_file = create_new_excel(excel_file)
         df = counterparty_name_add_to_df(excel_file)
         if len(df) > 0:
             df = doc_tax_details_add_to_df(df)
@@ -208,6 +224,9 @@ def add_other_parameters_to_df(excel_file):
                         df['Сумв_ПДВ'] = df['Сумв_ПДВ'].astype(float).apply(lambda x: round(x, 2))
                         df['номерРеализации'] = df['номерРеализации'].apply(int)
                         df['датаРеализации'] = df['датаРеализации'].apply(pd.to_datetime, format='%d.%m.%Y')
+
+        df = ttn_add_to_df(df)
+        df['НомерТТН_и_ВН_1С'] = np.where(df['номерРеализации'] == df['1СномерТТН'], '', ['Не совпадает'])
 
     except Exception as e:
         print(str(e))
@@ -265,6 +284,7 @@ def convert_date_to_str_df(df, column_name):
 # source_from_excel_df = df with data Vika + add other columns
 # pdf_df - pdf file names (ВН, ТТН)
 def merge_excel_and_word(source_from_excel_df, pdf_df, excel_dir, date_of_payments):
+    pdf_df = pdf_df.astype(str)
     # df = pd.read_excel(excel_file_source, sheet_name='df')
     source_from_excel_df = source_from_excel_df.astype(str)
     template = r"\\PRESTIGEPRODUCT\Scan\Maket.docx"
@@ -390,7 +410,8 @@ def get_bank_statement(date_of_payments):
         else:
             result += f"{i + 4}. Банківська виписка від {'{:%d.%m.%Y}'.format(date)}р."
     else:
-        print("Выписки банка в формате pdf в текущем каталоге не обнаружены")
+        print("Выписки банка в формате pdf в текущем каталоге не обнаружены\n"
+              "В док 'Лист пояснення' инф о платежах будет отсутствовать")
     return result
 
 
@@ -398,12 +419,16 @@ def merge_excle_word_main(excel_file):
     if not os.path.exists(excel_file):
         print("Не найден Excel файл", excel_file)
         sys.exit(0)
-        
+
+    # creating df with pdf files
     excel_dir = Path(os.path.dirname(excel_file))
     pdf_files_df, date_of_payment = get_pdf_set_with_date_in_file_name(excel_dir)
+
+    #
     date_of_payments = get_bank_statement(date_of_payment)
+
+    # add other column to source - excel + adding df with pdf files
     df_merge = edit_excel_and_return_df(excel_file, pdf_files_df)
-    pdf_files_df = pdf_files_df.astype(str)
     merge_excel_and_word(df_merge, pdf_files_df, excel_dir, date_of_payments)
 
 
--- TTN.py
+++ TTN.py
@@ -2,8 +2,38 @@
 # "ДокументОснование_Type": "StandardODATA.Document_РеализацияТоваровУслуг"
 # "ДокументОснование_Type": "StandardODATA.Document_скЛистДоставки"
 
-# find numer TTN by number invoice
+# find numer TTN by invoice_key
+import os
+import sys
+import requests
+import json
+
+if os.environ['COMPUTERNAME'] == 'PRESTIGEPRODUCT':
+    CONFIG_PATH = r"D:\Prestige\Python\Config"
+else:
+    CONFIG_PATH = r"C:\Rasim\Python\Config"
+
+sys.path.append(os.path.abspath(CONFIG_PATH))
+from configPrestige import DATA_AUTH
 
 
-def get_find_doc_base_type(invoice_number):
-    pass
+def get_ttn_details(invoice_key):
+    result = ''
+    url = ("http://192.168.1.254/utp_prestige/odata/standard.odata/Document_скТоварноТранспортнаяНакладная?"
+           "$format=json&$top=10&$inlinecount=allpages&$select=Date,Number"
+           f"&$filter=Товары/ДокументОтгрузки eq cast(guid'{invoice_key}',"
+           "'Document_РеализацияТоваровУслуг')&$orderby=Date desc")
+    try:
+        response = requests.get(url, auth=DATA_AUTH)
+        if response.status_code == 200:
+            result = response.json()['value'][0]
+    except:
+        print(f"Возникла ошибка при получении данных из url: {url}")
+
+    finally:
+        return result
+
+
+
+if __name__ == '__main__':
+    get_ttn_details('8b108495-507d-11ee-8195-001dd8b72b55')
